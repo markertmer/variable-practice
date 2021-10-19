595f9cfd81bde569eb9eb8159723c0af71c10178
Mark Ertmer <mark.ertmer@gmail.com>
2021-10-19 10:01:06 -0600

    Add hash variables

--- model-1.rb
+++ model-1.rb
@@ -14,3 +14,25 @@ has_dogs = true
 pet_names = ['Lola','Tommy','Hans','Athena']
 schools_attended = ['Jefferson H.S.','Purdue University','Ball State','UW Milwaukee']
 bike_types = ['road','gravel','cruiser','mountain','fatbike']
+
+favorites = {tv_show:'Trailer Park Boys',musician:'Judas Priest',summer_activity:'camping',winter_activity:'sledding'}
+careers = {education:'high school science teacher',marketing:'copywriter',software:'back end student'}
+talents = {music:'guitar',visual_art:'illustration',athletics:'cycling'}
+
+p favorites
+p " "
+p careers
+p " "
+p talents
+p " "
+print favorites
+print " "
+print careers
+print " "
+print talents
+print " "
+puts favorites
+puts " "
+puts careers
+puts " "
+puts talents
