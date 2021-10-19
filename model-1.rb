111f7f10ee51d956fe82ffc19965120cd7a4523f
Mark Ertmer <mark.ertmer@gmail.com>
2021-10-19 09:51:28 -0600

    Add array variables

--- model-1.rb
+++ model-1.rb
@@ -10,3 +10,7 @@ is_tired = true
 is_bored = false
 has_cats = true
 has_dogs = true
+
+pet_names = ['Lola','Tommy','Hans','Athena']
+schools_attended = ['Jefferson H.S.','Purdue University','Ball State','UW Milwaukee']
+bike_types = ['road','gravel','cruiser','mountain','fatbike']
